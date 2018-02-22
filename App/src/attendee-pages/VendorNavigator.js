84f9b131d2e227af7b909545e750cab32d929d76
Student User <jacob.devera12@gmail.com>
2018-02-22 02:43:29 -0800

    Added initial vendor food list

--- App/src/attendee-pages/VendorNavigator.js
+++ App/src/attendee-pages/VendorNavigator.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Image, View } from 'react-native';
-import { Button, Container, Content, Card, CardItem, Body, Text, Icon, Left, Right, Thumbnail, List } from 'native-base';
+import { Button, Container, Content, Card, CardItem, Body, Text, Icon, Left, Right, Thumbnail, List, ListItem } from 'native-base';
 import { StackNavigator } from "react-navigation";
 
 
@@ -13,19 +13,54 @@ const VendorList = (props) => {
             name: 'Baguette Box',
             desc: 'Specializing in western Chinese fare',
             img: require('../../img/bubble-tea.jpg'),
-            menu: ['Cumin Beef Sandwich', 'Cold Rice Noodles', 'Grilled Squid']
+            menu: [
+                { 
+                    name: 'Cumin Beef Sandwich',
+                    price: 3
+                },
+                { 
+                    name: 'Cold Rice Noodles',
+                    price: 5
+                },
+                { 
+                    name: 'Grilled Squid',
+                    price: 7
+                }
+            ]
         },
         {
             name: 'BeanFish',
             desc: "Seattle's only mobile taiyaki (鯛焼き) artisans",
             img: require('../../img/fish-tofu.jpg'),
-            menu: ['Cumin Beef SandWich', 'Cold Rice Noodles', 'Grilled Squid']
+            menu: [
+                { 
+                    name: 'Jiro Taiyaki',
+                    price: 6
+                },
+                { 
+                    name: 'Kpop Taiyaki',
+                    price: 7
+                }
+            ]
         },
         {
             name: 'Capuli Club Sips & Treats',
             desc: 'Artisanal fruit snacks made in Seattle',
             img: require('../../img/bbq-pork-buns.jpg'),
-            menu: ['Cumin Beef SandWich', 'Cold Rice Noodles', 'Grilled Squid']
+            menu: [
+                { 
+                    name: 'BBQ Pork Buns',
+                    price: 5
+                },
+                { 
+                    name: 'Fish Tofu',
+                    price: 4
+                },
+                { 
+                    name: 'Taiwanese Sausage',
+                    price: 3
+                }
+            ]
         }
     ];
 
@@ -38,9 +73,16 @@ const VendorList = (props) => {
                 <List
                     dataArray={items}
                     renderRow={(data) => {
+                        let itemNames = [];
+                        data.menu.forEach((item) => itemNames.push(item.name));
                         return (
                             <Card>
-                            <CardItem button onPress={() => { props.navigation.navigate('VendorFood'); }}>
+                            <CardItem 
+                                button onPress={() => { props.navigation.navigate('VendorFood', {
+                                        vendor: data
+                                    }); 
+                                }}
+                            >
                                 <Left>
                                     <Thumbnail
                                         square
@@ -51,7 +93,7 @@ const VendorList = (props) => {
                                 <Body>
                                     <Text style={ [styles.header, styles.cardH1] }>{data.name}</Text>
                                     <Text style={ [styles.desc] }>{data.desc}</Text>
-                                    <Text style={styles.bold}>Menu: <Text style={styles.menuItem}>{data.menu.join(', ')}</Text></Text>
+                                    <Text style={styles.bold}>Menu: <Text style={styles.menuItem}>{itemNames.join(', ')}</Text></Text>
                                 </Body>
                             </CardItem>
                             </Card>
@@ -62,16 +104,72 @@ const VendorList = (props) => {
         </Container>);
 }
 
-const VendorFood = (props) => {
-    return (
-        <Container>
-            <StackHeader navigation={props.navigation}>
-                Vendors / Food
-            </StackHeader>
-            <Content style={styles.paddedContainer}>
-            </Content>
-        </Container>
-    );
+class VendorFood extends Component {
+    constructor() {
+        super();
+        this.state = {
+            vendor: null,
+            order: []
+        }
+    }
+
+    componentDidMount() {
+        const { params } = this.props.navigation.state;
+        const vendor = params ? params.vendor : null;
+        let order = new Array(vendor.menu.length);
+        vendor.menu.forEach((item, index) => {
+            order[index] = { name: item.name, price: item.price, quantity: 0};
+        })
+        this.setState({ vendor: vendor, order: order });
+    }
+
+    updateQuantity(index, add) {
+        let order = this.state.order.slice();
+        order[index].quantity += add ? 1 : -1;
+        this.setState({ order: order })
+    }
+    
+    render() {
+        let { vendor, order } = this.state;
+        let totalQuantity = 0;
+        let totalPrice = 0;
+        order.forEach((item) => {
+            totalQuantity += item.quantity;
+            totalPrice += item.quantity * item.price;
+        });
+        return (
+            <Container>
+                <StackHeader navigation={this.props.navigation}>
+                    {vendor && vendor.name}
+                </StackHeader>
+                <Content style={styles.paddedContainer}>
+                    {vendor &&
+                        <List
+                            key={'quantity ' + totalQuantity}
+                            dataArray={vendor.menu}
+                            renderRow={(item, sectionID, rowID) => {
+                                return (
+                                        <ListItem>
+                                            <Left>
+                                                <Button onPress={() => this.updateQuantity(rowID, false)}><Text>-</Text></Button>
+                                                <Text>{order[rowID].quantity}</Text>
+                                                <Button onPress={() => this.updateQuantity(rowID, true)}><Text>+</Text></Button>
+                                            </Left>
+                                            <Body>
+                                                <Text>{item.name}</Text>
+                                            </Body>
+                                            <Right>
+                                                <Text>${item.price}</Text>
+                                            </Right>
+                                        </ListItem>
+                                )
+                            }}
+                        />}
+                    <Text style={[styles.center, styles.bold]}>Total Due: {totalPrice}</Text>
+                </Content>
+            </Container>
+        );
+    }
 }
 
 const VendorNavigator = StackNavigator(
@@ -84,4 +182,4 @@ const VendorNavigator = StackNavigator(
     }
 );
 
-export { VendorNavigator, VendorFood }
+export { VendorNavigator }
