46dc084aebc67a3e206b3c4fbe5a96876c891b99
Zhaoyang Yi <zhaoyangyi@Zhaoyangs-MacBook-Pro.local>
2018-02-23 04:06:57 -0800

    adding login functions

--- App/src/App.js
+++ App/src/App.js
@@ -8,14 +8,35 @@ import commonColor from './native-base-theme/variables/commonColor';
 import { AttendeeDrawerNav, VendorDrawerNav } from './routes';
 import { AppHeader, Sidebar } from './components';
 import { LandingPage } from './LandingPage';
+import firebase from 'firebase';
+
 
 export default class App extends Component {
    constructor() {
       super();
       this.state = {
-         view: 0 // 0 = neither, 1 = attendee, 2 = vendor
+         view: 0, // 0 = neither, 1 = attendee, 2 = vendor
+         loggedIn: null
       };
    }
+
+   componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD9oIu2WqxmvQOrIlPQ-7GQRKZJgKamSk4",
+      authDomain: "uwnightmarket-90946.firebaseapp.com",
+      databaseURL: "https://uwnightmarket-90946.firebaseio.com",
+      storageBucket: "uwnightmarket-90946.appspot.com",
+      messagingSenderId: "119944110578"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
 
    setView = (index) => {
       this.setState({ view: index });
