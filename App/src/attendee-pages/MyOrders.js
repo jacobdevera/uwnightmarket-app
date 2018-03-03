02022b6a51ed4ecc632803c294d6815f26dda056
Student User <jacob.devera12@gmail.com>
2018-03-03 01:52:49 -0800

    Add getOrders function

--- App/src/attendee-pages/MyOrders.js
+++ App/src/attendee-pages/MyOrders.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Image, View } from 'react-native';
 import { Button, Container, Content, H1, H2, H3, Text } from 'native-base';
+import firebase from 'firebase';
 
 import { AppHeader } from '../components';
 import styles from '../styles';
@@ -13,18 +14,32 @@ class MyOrders extends Component {
 
     componentDidMount() {
         let orderRef = firebase.database().ref('/orders').orderByKey();
+        firebase.auth().currentUser.getIdToken(true).then((token) => {
+            fetch('https://us-central1-uwnightmarket-90946.cloudfunctions.net/getOrders', { 
+                method: 'GET', 
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+            }).then((response) => {
+                    return response.json();
+            }).then((data) => {
+                    console.log(data);
+            });
+        }).catch(function (err) {
+            console.error(err);
+        });
         
     }
 
     render() {
         return (
             <Container>
-                <AppHeader navigation={props.navigation}>
+                <AppHeader navigation={this.props.navigation}>
                     My Orders
                 </AppHeader>
                 <View style={styles.column}>
                     <Text>You have no active orders right now.</Text>
-                    <Button style={{ alignSelf: 'center' }} onPress={() => props.navigation.navigate('VendorNavigator')}>
+                    <Button style={{ alignSelf: 'center' }} onPress={() => this.props.navigation.navigate('VendorNavigator')}>
                         <Text>Order Now</Text>
                     </Button>
                 </View>
