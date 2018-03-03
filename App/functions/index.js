180fdab688af18665c7ae63b4d911c6d3995eec3
Student User <jacob.devera12@gmail.com>
2018-03-03 01:52:49 -0800

    Add getOrders function

--- App/functions/index.js
+++ App/functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+// get attendee-specific orders using their token to get their id
+exports.getOrders = functions.https.onRequest((req, res) => {
+    let ordersRef = admin.database().ref('/orders');
+    const idToken = req.get('Authorization').split('Bearer ')[1];
+
+    return admin.auth().verifyIdToken(idToken).then((decodedToken) => {
+        let uid = decodedToken.uid;
+        return ordersRef.once('value', (snapshot) => {
+            let orders = [];
+            snapshot.forEach((vendorSnapshot) => {
+                vendorSnapshot.forEach((orderSnapshot) => {
+                    let order = orderSnapshot.val();
+                    if (order.userId === uid) {
+                        orders.push(order);
+                    }
+                });
+            });
+            return res.status(200).send(orders);
+        });
+        }).catch((error) => {
+            console.log(error);
+        }
+    );
+})
