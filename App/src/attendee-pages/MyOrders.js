aedfed365e60416d7acaac435da0b14caacfc68e
Student User <jacob.devera12@gmail.com>
2018-03-02 16:23:55 -0800

    Add Firebase funcitons

--- App/src/attendee-pages/MyOrders.js
+++ App/src/attendee-pages/MyOrders.js
@@ -5,19 +5,31 @@ import { Button, Container, Content, H1, H2, H3, Text } from 'native-base';
 import { AppHeader } from '../components';
 import styles from '../styles';
 
-const MyOrders = (props) => {
-    return (
-        <Container>
-            <AppHeader navigation={props.navigation}>
-                My Orders
-            </AppHeader>
-            <View style={styles.column}>
-                <Text>You have no active orders right now.</Text>
-                <Button style={{ alignSelf: 'center' }} onPress={() => props.navigation.navigate('VendorNavigator')}>
-                    <Text>Order Now</Text>
-                </Button>
-            </View>
-        </Container>);
+class MyOrders extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { orders: [] }
+    }
+
+    componentDidMount() {
+        let orderRef = firebase.database().ref('/orders').orderByKey();
+        
+    }
+
+    render() {
+        return (
+            <Container>
+                <AppHeader navigation={props.navigation}>
+                    My Orders
+                </AppHeader>
+                <View style={styles.column}>
+                    <Text>You have no active orders right now.</Text>
+                    <Button style={{ alignSelf: 'center' }} onPress={() => props.navigation.navigate('VendorNavigator')}>
+                        <Text>Order Now</Text>
+                    </Button>
+                </View>
+            </Container>);
+    }
 }
 
 export { MyOrders }
