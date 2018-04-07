cae1ebc9f34f5444c3fa0e8dbdffbf496282a79a
Jacob Devera <jacob.devera12@gmail.com>
2018-04-07 01:07:15 -0700

    Improve performance by only downloading updated orders on vendor side

--- App/src/components/OrderList.js
+++ App/src/components/OrderList.js
@@ -63,7 +63,7 @@ export class OrderList extends Component {
         let res = "";
         let index = 6;
         let alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        while (count > 0) {
+        while (count > 0 && index < input.length) {
             let c = input[index];
             if (alphabet.indexOf(c) != -1) {
                 res += c;
